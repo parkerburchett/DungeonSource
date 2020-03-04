82c4bc3ff523dca8cf2b2f9faadcf35639575046
chris <davissonchris@gmail.com>
2020-03-03 17:24:25 -0800

    Big Things
    Removed the comments
    removed the need for keyboard
    Added Scanner
    Added Scanner to methods that need input
    cleaned up bad code like switch statements
    moved the print menu to its own class for readability

--- DungeonSource/Dungeon.java
+++ DungeonSource/Dungeon.java
@@ -1,144 +1,95 @@
-/// YOU ARE NOW COMITING THE FIRST CHANGE
+import java.util.Scanner;
 
-
-/**
- * Title: Dungeon.java
- *
- * Description: Driver file for Heroes and Monsters project
- *
- * Copyright:    Copyright (c) 2001
- * Company: Code Dogs Inc.
- * I.M. Knurdy
- *
- * History:
- *  11/4/2001: Wrote program
- *    --created DungeonCharacter class
- *    --created Hero class
- *    --created Monster class
- *    --had Hero battle Monster
- *    --fixed attack quirks (dead monster can no longer attack)
- *    --made Hero and Monster abstract
- *    --created Warrior
- *    --created Ogre
- *    --made Warrior and Ogre battle
- *    --added battleChoices to Hero
- *    --added special skill to Warrior
- *    --made Warrior and Ogre battle
- *    --created Sorceress
- *    --created Thief
- *    --created Skeleton
- *    --created Gremlin
- *    --added game play features to Dungeon.java (this file)
- *  11/27/2001: Finished documenting program
- * version 1.0
- */
-
-
-
-/*
-  This class is the driver file for the Heroes and Monsters project.  It will
-  do the following:
-
-  1.  Allow the user to choose a hero
-  2.  Randomly select a monster
-  3.  Allow the hero to battle the monster
-
-  Once a battle concludes, the user has the option of repeating the above
-
-*/
 public class Dungeon
 {
     public static void main(String[] args)
 	{
+		Scanner kb = new Scanner(System.in);
 
 		Hero theHero;
 		Monster theMonster;
 
 		do
 		{
-		    theHero = chooseHero();
+		    theHero = chooseHero(kb);
 		    theMonster = generateMonster();
-			battle(theHero, theMonster);
+			battle(theHero, theMonster , kb);
 
-		} while (playAgain());
+		} while (playAgain(kb));
 
     }//end main method
 
-/*-------------------------------------------------------------------
-chooseHero allows the user to select a hero, creates that hero, and
-returns it.  It utilizes a polymorphic reference (Hero) to accomplish
-this task
----------------------------------------------------------------------*/
-	public static Hero chooseHero()
+	public static Hero chooseHero(Scanner kb)
 	{
-		int choice;
-		Hero theHero;
+		printMenu();
+		return  getHeroChoice(kb);
+	}
 
+	private static void printMenu(){
 		System.out.println("Choose a hero:\n" +
-					       "1. Warrior\n" +
-						   "2. Sorceress\n" +
-						   "3. Thief");
-		choice = Keyboard.readInt();
+				"1. Warrior\n" +
+				"2. Sorceress\n" +
+				"3. Thief");
+	}
 
-		switch(choice)
-		{
-			case 1: return new Warrior();
+	private static Hero getHeroChoice(Scanner kb){
+		int choice = getChoice(kb , 1 , 3);
+		if(choice == 1){
+			return new Warrior();
+		}
+		else if(choice == 2){
+			return new Sorceress();
+		}
+		else{
+			return new Thief();
+		}
+	}
 
-			case 2: return new Sorceress();
+	private static int getChoice(Scanner kb , int lowerbounds , int upperbounds){
+		int input = -1;
+		while(input < lowerbounds && input > upperbounds){
 
-			case 3: return new Thief();
+			try{
+				input = kb.nextInt();
+			}catch (Exception e){
+				System.out.println("invalid number");
+			}
+		}
+		return input;
+	}
 
-			default: System.out.println("invalid choice, returning Thief");
-				     return new Thief();
-		}//end switch
-	}//end chooseHero method
 
-/*-------------------------------------------------------------------
-generateMonster randomly selects a Monster and returns it.  It utilizes
-a polymorphic reference (Monster) to accomplish this task.
----------------------------------------------------------------------*/
 	public static Monster generateMonster()
 	{
 		int choice;
 
 		choice = (int)(Math.random() * 3) + 1;
 
-		switch(choice)
-		{
-			case 1: return new Ogre();
+		if(choice  == 1){
+			return new Ogre();
+		}
+		else if(choice == 2){
+			return new Gremlin();
+		}
+		else{
+			return new Skeleton();
+		}
 
-			case 2: return new Gremlin();
+	}
 
-			case 3: return new Skeleton();
-
-			default: System.out.println("invalid choice, returning Skeleton");
-				     return new Skeleton();
-		}//end switch
-	}//end generateMonster method
-
-/*-------------------------------------------------------------------
-playAgain allows gets choice from user to play another game.  It returns
-true if the user chooses to continue, false otherwise.
----------------------------------------------------------------------*/
-	public static boolean playAgain()
+	public static boolean playAgain(Scanner kb)
 	{
-		char again;
 
 		System.out.println("Play again (y/n)?");
-		again = Keyboard.readChar();
+		char again = kb.nextLine().toLowerCase().charAt(0);
 
-		return (again == 'Y' || again == 'y');
+		return (again == 'y');
 	}//end playAgain method
 
 
-/*-------------------------------------------------------------------
-battle is the actual combat portion of the game.  It requires a Hero
-and a Monster to be passed in.  Battle occurs in rounds.  The Hero
-goes first, then the Monster.  At the conclusion of each round, the
-user has the option of quitting.
----------------------------------------------------------------------*/
-	public static void battle(Hero theHero, Monster theMonster)
-	{
+	//Battle method, hero goes first and ends when user quits or something dies
+	public static void battle(Hero theHero, Monster theMonster , Scanner kb)
+ 	{
 		char pause = 'p';
 		System.out.println(theHero.getName() + " battles " +
 							theMonster.getName());
@@ -150,13 +101,13 @@ user has the option of quitting.
 		    //hero goes first
 			theHero.battleChoices(theMonster);
 
-			//monster's turn (provided it's still alive!)
+			//monster's turn
 			if (theMonster.isAlive())
 			    theMonster.attack(theHero);
 
-			//let the player bail out if desired
+
 			System.out.print("\n-->q to quit, anything else to continue: ");
-			pause = Keyboard.readChar();
+			pause = kb.next().toLowerCase().charAt(0);
 
 		}//end battle loop
 
@@ -167,7 +118,5 @@ user has the option of quitting.
 		else//both are alive so user quit the game
 			System.out.println("Quitters never win ;-)");
 
-	}//end battle method
-
-
-}//end Dungeon class
+	}
+}
