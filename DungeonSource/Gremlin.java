853436911059bccf3aad977fdc505ad618eebe78
Parker Burchett <parkerburchett@gmail.com>
2020-03-05 11:19:18 -0800

    Bug Fixed Constructor
    Fixed Ogre and Gremlin Construtors

--- DungeonSource/Gremlin.java
+++ DungeonSource/Gremlin.java
@@ -2,7 +2,7 @@ public class Gremlin extends Monster
 {
     public Gremlin()
 	{
-		super("Gnarltooth the Gremlin", 70, 5, .8, .4, 15, 30, , " jabs his kris at " 20, 40 );
+		super("Gnarltooth the Gremlin", 70, 5, .8, .4, 15, 30, " jabs his kris at ", 20, 40 );
     }
 }
 
