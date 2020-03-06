281ced57f9edb4d50a9335bb80b3feed43b56f69
Christopher Davisson <davissonchris@gmail.com>
2020-03-06 11:36:56 -0800

    removed final comments

--- DungeonSource/Ogre.java
+++ DungeonSource/Ogre.java
@@ -5,7 +5,3 @@ public class Ogre extends Monster
 		super("Oscar the Ogre", 200, 2, .6, .1, 30, 50, " slowly swings a club toward's ", 30, 50);
     }
 }
-
-// added ATTACK_DISCRIPTION as a feild passed into the constructor.
-// removed @Override Attack
-// removed useless comments
