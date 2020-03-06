cc428b5d3bdbb69a14d46a3d133b4b59731536d1
Christopher Davisson <davissonchris@gmail.com>
2020-03-06 11:34:09 -0800

    removed final comments

--- DungeonSource/Gremlin.java
+++ DungeonSource/Gremlin.java
@@ -5,7 +5,3 @@ public class Gremlin extends Monster
 		super("Gnarltooth the Gremlin", 70, 5, .8, .4, 15, 30, " jabs his kris at ", 20, 40 );
     }
 }
-
-// added ATTACK_DISCRIPTION as a feild passed into the constructor.
-// removed @Override Attack
-// removed useless comments
