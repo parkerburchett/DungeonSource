c8a619756e09309474e2da772ab4d9d03f01769a
Christopher Davisson <davissonchris@gmail.com>
2020-03-18 16:54:11 -0700

    random things added

--- DungeonSource/Maze.java
+++ DungeonSource/Maze.java
@@ -8,6 +8,7 @@ public class Maze {
         private boolean entranceMade = false;
         public boolean exitMade = false;
 
+
         public Maze(Hero theHero){
             maze = buildDungeon();
             this.theHero = theHero;
@@ -15,31 +16,31 @@ public class Maze {
 
         public Room[][] buildDungeon(){
 
-            Room c0r0 = new Room(null ,new Entrance() ,0,0);
-            Room c0r1 = new Room(null ,new Exit() ,1,0);
-            Room c0r2 = new Room(null ,null ,2,0);
-            Room c0r3 = new Room(null ,null ,3,0);
-            Room c0r4 = new Room(null ,null ,4,0);
-            Room c1r0 = new Room(null ,null ,0,1);
-            Room c1r1 = new Room(null ,null ,1,1);
-            Room c1r2 = new Room(null ,null ,2,1);
-            Room c1r3 = new Room(null ,null ,3,1);
-            Room c1r4 = new Room(null ,null ,4,1);
-            Room c2r0 = new Room(null ,null ,0,2);
-            Room c2r1 = new Room(null ,null ,1,2);
-            Room c2r2 = new Room(null ,null ,2,2);
-            Room c2r3 = new Room(null ,null ,3,2);
-            Room c2r4 = new Room(null ,null ,4,2);
-            Room c3r0 = new Room(null ,null ,0,3);
-            Room c3r1 = new Room(null ,null ,1,3);
-            Room c3r2 = new Room(null ,null ,2,3);
-            Room c3r3 = new Room(null ,null ,3,3);
-            Room c3r4 = new Room(null ,null ,4,3);
-            Room c4r0 = new Room(null ,null ,0,4);
-            Room c4r1 = new Room(null ,null ,1,4);
-            Room c4r2 = new Room(null ,null ,2,4);
-            Room c4r3 = new Room(null ,null ,3,4);
-            Room c4r4 = new Room(null ,null ,4,4);
+            Room c0r0 = new Room(addMonster() ,entranceExit(0,0) ,0,0);
+            Room c0r1 = new Room(addMonster() ,entranceExit(0,1) ,1,0);
+            Room c0r2 = new Room(addMonster() ,entranceExit(0,2) ,2,0);
+            Room c0r3 = new Room(addMonster() ,entranceExit(0,3) ,3,0);
+            Room c0r4 = new Room(addMonster() ,entranceExit(0,4) ,4,0);
+            Room c1r0 = new Room(addMonster() ,entranceExit(1,0) ,0,1);
+            Room c1r1 = new Room(addMonster() ,entranceExit(1,1) ,1,1);
+            Room c1r2 = new Room(addMonster() ,entranceExit(1,2) ,2,1);
+            Room c1r3 = new Room(addMonster() ,entranceExit(1,3) ,3,1);
+            Room c1r4 = new Room(addMonster() ,entranceExit(1,4) ,4,1);
+            Room c2r0 = new Room(addMonster() ,entranceExit(2,0) ,0,2);
+            Room c2r1 = new Room(addMonster() ,entranceExit(2,1) ,1,2);
+            Room c2r2 = new Room(addMonster() ,entranceExit(2,2) ,2,2);
+            Room c2r3 = new Room(addMonster() ,entranceExit(2,3) ,3,2);
+            Room c2r4 = new Room(addMonster() ,entranceExit(2,4) ,4,2);
+            Room c3r0 = new Room(addMonster() ,entranceExit(3,0) ,0,3);
+            Room c3r1 = new Room(addMonster() ,entranceExit(3,1) ,1,3);
+            Room c3r2 = new Room(addMonster() ,entranceExit(3,2) ,2,3);
+            Room c3r3 = new Room(addMonster() ,entranceExit(3,3) ,3,3);
+            Room c3r4 = new Room(addMonster() ,entranceExit(3,4) ,4,3);
+            Room c4r0 = new Room(addMonster() ,entranceExit(4,0) ,0,4);
+            Room c4r1 = new Room(addMonster() ,entranceExit(4,1) ,1,4);
+            Room c4r2 = new Room(addMonster() ,entranceExit(4,2) ,2,4);
+            Room c4r3 = new Room(addMonster() ,entranceExit(4,3) ,3,4);
+            Room c4r4 = new Room(addMonster() ,entranceExit(4,4) ,4,4);
             Room[][] theMaze = new Room[][]{{c0r0, c0r1, c0r2, c0r3, c0r4}, {c1r0, c1r1, c1r2, c1r3, c1r4},
                     {c2r0, c2r1, c2r2, c2r3, c2r4}, {c3r0, c3r1, c3r2, c3r3, c3r4},
                     {c4r0, c4r1, c4r2, c4r3, c4r4}};
@@ -63,9 +64,53 @@ public class Maze {
             return this.currrentLocation.getMonster();
         }
 
-        private Entrance hasEntrance(){
-            if(!entranceMade)
+        public RoomThingDesc entranceExit(int x, int y){
+            if(entranceMade && exitMade)
+                return null;
+            if(entranceMade && !exitMade && getRandomChance(10)){
+                maze[x][y].setMonsterNull();
+                return new Exit();
+            }
+            if(!entranceMade && exitMade && getRandomChance(10)){
+                maze[x][y].setMonsterNull();
                 return new Entrance();
+            }
+            if(x == 4 && y == 4 && !entranceMade){
+                maze[x][y].setMonsterNull();
+                return new Entrance();
+            }
+            if(x == 4 && y == 3 && !exitMade){
+                maze[x][y].setMonsterNull();
+                return new Exit();
+            }
             return null;
+        }
+
+        public Monster addMonster(){
+            if(getRandomChance(25)){
+                return randomMonster();
+            }
+            return null;
+        }
+
+        public Monster randomMonster(){
+            int random = (int)Math.random() * 5;
+            if(random == 1)
+                return new Skeleton();
+            else if(random == 2)
+                return new Gremlin();
+            else if(random == 3)
+                return new Ogre();
+            else if(random == 4)
+                return new Gremlin();
+            else
+                return new Dragon();
+        }
+
+        private boolean getRandomChance(int percentChance){
+            int random = (int)Math.random() * 100;
+            if(percentChance > random)
+                return true;
+            return false;
         }
 }
