d1af1d1fa94d789353e4e3b6799ad04c7c0afe2a
Parker Burchett <parkerburchett@gmail.com>
2020-03-18 16:52:37 -0700

    Updated Room to be clean

--- DungeonSource/Room.java
+++ DungeonSource/Room.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 // A room will never have more than more than 2 things
 public class Room
 {
-    private RoomThingDesc door; // hold exit or entrence;
-    private Monster monster;
-    public int xCord;
-    public int yCord;
-
+   private RoomThingDesc door; // hold exit or entrence;
+   private Monster monster;
+   public int xCord;
+   public int yCord;
+    
    public Room(Monster monster, RoomThingDesc door, int xCord, int yCord )
    {
       this.monster = monster;
@@ -15,21 +15,20 @@ public class Room
       this.xCord = xCord;
       this.yCord = yCord;
    }
-
-   @Override
+   
+   public Monster getMonster(){return this.monster;}
+   public int xCord() {return this.xCord;}
+   public int yCord() {return this.yCord;}
+   
    public String toString()
    {
-      return null;
+      return "";
    }
    
     
    public void enter(Hero hero)
    {
-      // adjust the hero's stats here eg vision potion and where the monster batle happens
-      // for checking conditions in the room run a this.toString().contains(keyword) on the room has a pit or a monster or a healthPotion.
-      // this do the hero.interact() in the enters method
       String roomString = this.toString();
-      
       if (roomString.contains("vision")){hero.numVisionPotions++;}
       if (roomString.contains("health")){hero.numHealthPotions++;}
       if (roomString.contains("pit")){hero.encountersPit();}
@@ -43,5 +42,5 @@ public class Room
       String visualizedRoom = RoomEdges.chooseRoom(xCord,yCord, center);
    }
 
-   public Monster getMonster(){return this.monster;}
+   
 }
